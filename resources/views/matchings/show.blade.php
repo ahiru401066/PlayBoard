c654e40538e8f81b7a410320a6fcff0a4d42ad3d
EC2 Default User <ichikawatomoya0705@gmail.com>
2023-11-08 16:54:23 +0900

    matching_show画面途中経過

--- resources/views/matchings/show.blade.php
+++ resources/views/matchings/show.blade.php
@@ -11,50 +11,68 @@
             Matching
         </x-slot>
         <body>
-            <h1>Matching</h1>
-            <div>
-                <p>{{ $matching->id }}</p>
-                <p>日時：{{ $matching->date }}</p>
-                <p>############################</p>
-                <p>参加ユーザー</p>
-                @foreach($matching->users as $user)
-                    <p>{{ $user->name }}</p>
-                @endforeach
-            </div>
-            <!--userの参加作成-->
-            <div>
-                @if ($is_join)
-                <form action="/matchings/{{ $matching->id}}/cancel" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <input type="submit" value="参加取り消し">
-                </form>
-                @else
-                <form action="/matchings/{{ $matching->id}}/join" method="POST">
-                    @csrf
-                    <input type="submit" value="参加する">
-                </form>
-                @endif
-            </div>
-            <!--userの意見交換作成-->
-            <div>
-                <form action="/matchings/{{ $matching->id }}/opinion" method="POST">
-                    @csrf
-                    <div class="comment_body">
-                        <h2>opinion</h2>
-                        <textarea name="opinion[opinion]" placeholder="コメントサンプル"></textarea>
+            <div class="m-3">
+                
+                <div>
+                    <p class="text-6xl">Matching部屋{{ $matching->id }}</p>
+                    <div class="text-right mr-20">
+                        <p class="text-2xl">日時：{{ $matching->date }}</p>
                     </div>
-                    <input type="submit" value="store">
-                </form>
-            </div>
-            <div>
-                @foreach($matching->opinions as $opinion)
-                    <p>{{ $opinion->user->name }}</p>
-                    <p>{{ $opinion->opinion}}</p>
-                @endforeach
-            </div>
-            <div class = "footer">
-                <a href="/matchings/index">戻る</a>
+                    
+                    <div class="flex justify-center">
+                        <!--userの意見交換作成-->
+                        <div>
+                            <form action="/matchings/{{ $matching->id }}/opinion" method="POST">
+                                @csrf
+                                <div class="comment_body">
+                                    <h2>opinion</h2>
+                                    <textarea name="opinion[opinion]" placeholder="コメントサンプル"></textarea>
+                                </div>
+                                <x-primary-button>
+                                    <input type="submit" value="保存" class="cursor-pointer text-slate-50">
+                                </x-primary-button>
+                            </form>
+                            <div>
+                                @foreach($matching->opinions as $opinion)
+                                    <p>{{ $opinion->user->name }}</p>
+                                    <p class="w-96">{{ $opinion->opinion}}</p>
+                                @endforeach
+                            </div>
+                        </div>
+                        
+                        <div>
+                            <p>参加ユーザー</p>
+                            @foreach($matching->users as $user)
+                                <p>{{ $user->name }}</p>
+                            @endforeach
+                            <!--userの参加作成-->
+                            @if ($is_join)
+                            <form action="/matchings/{{ $matching->id}}/cancel" method="POST">
+                                @csrf
+                                @method('DELETE')
+                                <x-primary-button>
+                                    <input type="submit" value="参加取り消し">
+                                </x-primary-button>
+                            </form>
+                            @else
+                            <form action="/matchings/{{ $matching->id}}/join" method="POST">
+                                @csrf
+                                <x-primary-button>
+                                    <input type="submit" value="参加する">
+                                </x-primary-button>
+                            </form>
+                            @endif
+                        </div>
+                        
+                    </div>
+                    
+                    <!--fooder-->
+                    <div class="text-right mr-20">
+                        <x-primary-button>
+                            <a href="/matchings/index">戻る</a>
+                        </x-primary-button>
+                    </div>
+                </div>
             </div>
         </body>
     </x-app-layout>
