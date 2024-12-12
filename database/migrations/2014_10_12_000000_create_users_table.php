a190a4dc5264f812ecbdb51e79ea55ce6ae376b8
Ichikawa Tomoya <101092289+ahiru401066@users.noreply.github.com>
2024-12-12 23:27:24 +0900

    Update 2014_10_12_000000_create_users_table.php
    migrationの役割付与修正

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->string('age',3)->nullable();
-            $table->integer('role')->nullable();
+            $table->integer('role')->default(0);
             $table->timestamps();
         });
     }
