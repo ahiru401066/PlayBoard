c9000c13bd5e1a6ff3990a7b8ba65ef45bcdbe7f
Ichikawa Tomoya <101092289+ahiru401066@users.noreply.github.com>
2024-12-12 23:29:30 +0900

    Update navigation.blade.php

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -13,13 +13,13 @@
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" class="text-green-800 hover:text-green-600">
-                        {{ __('Dashboard') }}
+                        {{ __('ダッシュボード') }}
                     </x-nav-link>
                     <x-nav-link :href="route('index')" :active="request()->routeIs('index')" class="text-green-800 hover:text-green-600">
-                        {{ __('Board Game') }}
+                        {{ __('ボードゲーム') }}
                     </x-nav-link>
                     <x-nav-link :href="route('matching')" :active="request()->routeIs('matching')" class="text-green-800 hover:text-green-600">
-                        {{ __('Matching') }}
+                        {{ __('マッチング') }}
                     </x-nav-link>
                     <!--<x-nav-link :href="route('map')" :active="request()->routeIs('map')" class="text-green-800 hover:text-green-600">-->
                     <!--    {{ __('Map') }}-->
