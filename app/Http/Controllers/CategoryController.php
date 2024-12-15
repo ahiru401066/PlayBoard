37b204fc404cc96f08a9b470e246a9744e47d341
Ichikawa Tomoya <101092289+ahiru401066@users.noreply.github.com>
2024-12-15 23:23:56 +0900

    Update CategoryController.php
    カテゴリー名前もviewに渡すように編集

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -11,7 +11,7 @@ class CategoryController extends Controller
     {
         return view('categories/index')->with([
             'games' => $category->getByCategory(),
-            'categoryName' => $category->name,
+            'categoryName' => $category->category,
         ]);
     }
 }
